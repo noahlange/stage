5d89f56e76a3921f2f0024827d69852c7e4bb12d
Noah Lange <Noah@Noahs-MacBook-Pro.local>
2014-09-14 23:31:03 -0500

    Added documentation to, cleaned up stage.js.

--- static/js/stage.js
+++ static/js/stage.js
@@ -1,14 +1,33 @@
+/*
+ * This is a long and boring page of miscellaneous code that makes everything work properly.
+ * There are a handful of things that may be of interest to an end user, namely the breakpoint
+ * variable at the top of the first function, which controls when the navbar is flipped back
+ * and forth from transparent to opaque due to mobile viewports or browser resizing. If you
+ * change things in _bootstrap_variables.scss, you're gonna want to change it here. This is
+ * in a separate section than the other stuff because no one wants to see the navbar flickering
+ * while everyone else takes their sweet time getting ready for the show.
+ */
+
 $("#cover-page, .article-cover").ready(function() {
+
+  var breakpoint = 768;
+  var transparentNavBackgroundColor = "rgba(0,0,0,0)";
+  var transparentNavLinkColor = "#fff";
+
   var defaultBackgroundColor = $(".navbar").css("background-color");
   var defaultBorderColor = $(".navbar").css("border-color");
   var defaultLinkColor = $(".navbar a").css("color");
+
+  var scrolledPastCover;
+
   function navbarTransparent() {
     $(".navbar").removeClass("navbar-default");
     $(".navbar").addClass("navbar-inverse");
-    $(".navbar").css("background-color", "rgba(0,0,0,0)");
-    $(".navbar").css("border-color", "rgba(0,0,0,0)");
-    $(".navbar a").css("color", "#fff");
+    $(".navbar").css("background-color", transparentNavBackgroundColor);
+    $(".navbar").css("border-color", transparentNavBackgroundColor);
+    $(".navbar a").css("color", transparentNavLinkColor);
   }
+
   function navbarOpaque() {
     $(".navbar").addClass("navbar-default");
     $(".navbar").removeClass("navbar-inverse");
@@ -16,17 +35,35 @@ $("#cover-page, .article-cover").ready(function() {
     $(".navbar").css("border-color", defaultBorderColor);
     $(".navbar a").css("color",defaultLinkColor);
   }
-  $( "#cover-page, .article-cover" ).waypoint(function(direction) {
-    navbarTransparent();
-  });
-  $( "#main-container" ).waypoint(function(direction) {
-    if (direction === 'up') {
-      navbarTransparent();
-    } else {
+
+  $(window).resize(function() {
+    if ($(window).width() < breakpoint) {
       navbarOpaque();
+    } else if (!scrolledPastCover) {
+      navbarTransparent();
     }
   });
-})
+  if ($(window).width() > breakpoint) {
+    $( "#cover-page, .article-cover" ).waypoint(function(direction) {
+      navbarTransparent();
+    });
+    $( "#main-container" ).waypoint(function(direction) {
+      if (direction === 'up') {
+        navbarTransparent();
+        scrolledPastCover = false;
+      } else {
+        navbarOpaque();
+        scrolledPastCover = true;
+      }
+    });
+  }
+});
+
+/*
+ * This next section is split between initializing stellar.js and all of the special
+ * case things that crop up with Tables of Contents, image modals and jPlayer. You're
+ * probably not going to need to mess with this.
+ */
 
 $(document).ready(function() {
 
@@ -42,13 +79,11 @@ $(document).ready(function() {
 
   if ($(".jp-jplayer").length != 0) {
     $(".jp-jplayer").each( function() {
-
       var parent = $(this).parent();
       var id = parent.attr('id');
       var datasrc = parent.attr('data-src');
       var title = parent.attr('title');
       var selector = "#jquery_jplayer_"+id;
-
       $( selector ).jPlayer({
         ready: function (event) {
           console.log("???")
